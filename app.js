a2f8f68e074f4c82c257ebbc8dd854acf558421a
dorgeo <36206566+dorgeo@users.noreply.github.com>
2018-08-17 14:06:57 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-document.getElementById("event-container").style.backgroundColor = "red";
+var shouldMenuShow=true
+document.getElementById("resources").addEventListener("click", displayMenu);
+
+
+function displayMenu() {
+if(shouldMenuShow) {
+  document.getElementById("menu").style.display = "block";
+  shouldMenuShow=false
+} else {
+  document.getElementById("menu").style.display = "none";
+  shouldMenuShow=true
+}
+
+}
